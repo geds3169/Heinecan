3ac440a68cf2caaaed471ed56f9cdb635e39294f
SCHLOSSER Guilhem <guilhemETkarine@hotmail.fr>
2026-04-03 18:10:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'heinecan-v1';
-const ASSETS_TO_CACHE = [
+const ASSETS = [
   './',
   './index.html',
   './style.css',
@@ -10,31 +10,10 @@ const ASSETS_TO_CACHE = [
   './icon-512.png'
 ];
 
-// Installation : Mise en cache des fichiers
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Activation : Nettoyage des anciens caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Stratégie : Cache d'abord, puis réseau
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
